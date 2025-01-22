4900d3715595558f464a3d8ec24d04900cc969ce
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2025-01-22 13:40:57 +0900

    Size Padding 追加

---
+++ lib/styles/size.dart
@@ -0,0 +1,9 @@
+class AppSize {
+  AppSize._();
+
+  /// Padding
+  static const double paddingS = 8;
+  static const double paddingM = 16;
+  static const double paddingL = 24;
+  static const double paddingXL = 32;
+}
