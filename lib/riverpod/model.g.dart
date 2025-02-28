87e43e7cd59a556bdbd4332438d2550cca232fea
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2025-02-28 18:49:05 +0900

    練習

---
+++ lib/riverpod/model.g.dart
@@ -0,0 +1,77 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$repositoryAliveHash() => r'4316cb198874ccdee1d02d2f07df996689bda4ab';
+
+/// See also [repositoryAlive].
+@ProviderFor(repositoryAlive)
+final repositoryAliveProvider = Provider<RepositoryInterface>.internal(
+  repositoryAlive,
+  name: r'repositoryAliveProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$repositoryAliveHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RepositoryAliveRef = ProviderRef<RepositoryInterface>;
+String _$cacheAliveHash() => r'bf29f68f7241663772f72445c80313f366d4035e';
+
+/// See also [cacheAlive].
+@ProviderFor(cacheAlive)
+final cacheAliveProvider = AutoDisposeProvider<String>.internal(
+  cacheAlive,
+  name: r'cacheAliveProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$cacheAliveHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CacheAliveRef = AutoDisposeProviderRef<String>;
+String _$cacheAlive2Hash() => r'417175259bb3e38e7c7389683c84e5e5259cbccc';
+
+/// See also [cacheAlive2].
+@ProviderFor(cacheAlive2)
+final cacheAlive2Provider = Provider<String>.internal(
+  cacheAlive2,
+  name: r'cacheAlive2Provider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$cacheAlive2Hash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CacheAlive2Ref = ProviderRef<String>;
+String _$repositoryDisposeHash() => r'c9ee04b60f8c1006f84e089b8720981e04bf7f4c';
+
+/// See also [repositoryDispose].
+@ProviderFor(repositoryDispose)
+final repositoryDisposeProvider =
+    AutoDisposeProvider<RepositoryInterface>.internal(
+  repositoryDispose,
+  name: r'repositoryDisposeProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$repositoryDisposeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RepositoryDisposeRef = AutoDisposeProviderRef<RepositoryInterface>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
